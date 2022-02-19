2edcfafe597c7708fcd771eca5dfca37b10d4621
Radhika <cu9rad@gmail.com>
2022-02-19 18:44:49 -0500

    get routes added

--- server.js
+++ server.js
@@ -12,3 +12,16 @@ const PORT = process.env.PORT || 3001;
 // Configures app to handle data parsing
 noteApp.use(express.urlencoded({ extended: true }));
 noteApp.use(express.json());
+
+// Route handling for HTML pages
+// Serves index.html
+noteApp.get('/', (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+
+// Serves CSS file for Notes page
+noteApp.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, "/public/assets/css/styles.css")));
+
+// Serves index.js file for Notes page
+noteApp.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, "/public/assets/js/index.js")));
+
+// Serves notes.html
+noteApp.get('/notes', (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
