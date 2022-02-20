280cdcd41414e5568e869bed177938a2f0965bae
Radhika <cu9rad@gmail.com>
2022-02-20 14:53:50 -0500

    note taker working

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-// Helper method for generating unique ids
-// const uuid = require('./helpers/uuid');
+
 const { v4: uuidv4 } = require('uuid');
-
-
 
 // Initialize express app
 const noteApp = express();
